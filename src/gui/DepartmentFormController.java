18d232ba3972c6d070af43d842a5d1000996f2e9
Robson Vellasques <robson.vellasques@gmail.com>
2020-12-06 22:37:53 -0300

    Observer e Listener

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -1,19 +1,22 @@
 package gui;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import db.DbException;
+import gui.listeners.DataChangeListener;
 import gui.util.Alerts;
 import gui.util.Constraints;
 import gui.util.utils;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.control.Alert.AlertType;
 import model.entities.Department;
 import model.services.DepartmentService;
 
@@ -22,6 +25,9 @@ public class DepartmentFormController implements Initializable {
 	private Department entity;
 	
 	private DepartmentService service;
+	
+	// Estou criando uma lista de objetos que desejam se inscrever para receber o evento de mudança de dados dessa classe (VIDE LISTENER / OBSERVER)
+	private List<DataChangeListener> dataChangeListeners = new ArrayList<>();
 	
 	@FXML
 	private TextField txtId;
@@ -50,6 +56,9 @@ public class DepartmentFormController implements Initializable {
 		this.service = service;
 	}
 
+	public void subscribeDataChangeListener(DataChangeListener listener) {   // Objetos vão se inscrever para receber eventos desta classe
+		dataChangeListeners.add(listener);
+	}
 	@FXML
 	public void onBtSaveAction(ActionEvent event) {
 //		System.out.println("onBtSaveAction");
@@ -64,6 +73,7 @@ public class DepartmentFormController implements Initializable {
 		try {
 			entity = getFormData();
 			service.saveOrUpdate(entity);
+			notifyDataChangeListeners();  // serve pra emitir evento na mudança de dados para todos os objetos da lista (classe que EMITE O EVENTO)
 			utils.currentStage(event).close();
 		}
 		catch (DbException e) {
@@ -71,6 +81,12 @@ public class DepartmentFormController implements Initializable {
 		}
 	}
 	
+	private void notifyDataChangeListeners() {
+		for (DataChangeListener listener : dataChangeListeners) {
+			listener.onDataChanged();
+		}
+	}
+
 	private Department getFormData() {          ///Pega os dados do formulário e retorna o objeto
 		Department obj = new Department();
 		
@@ -104,6 +120,6 @@ public class DepartmentFormController implements Initializable {
 		txtId.setText(String.valueOf(entity.getCodigo())); //Sempre tem que converter pra texto se necessário.
 		txtName.setText(entity.getNome());
 	}
-	
+
 
 }
